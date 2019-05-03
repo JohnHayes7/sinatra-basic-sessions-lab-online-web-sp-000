09abc9e8abf8a753e4d6de07aa2ad626afb09540
John Hayes <johnnyhayes7@gmail.com>
2019-05-03 16:58:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  configure do
-    enable :sessions
-    set :session_secret, "Miera"
-  end
+  # configure do
+  #   enable :sessions
+  #   set :session_secret, "Miera"
+  # end
+  
+  get '/' do
+    
+  erb :index
+  
 end
